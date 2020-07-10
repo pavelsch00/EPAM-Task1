2276a243aefe83da45933be8a737890dd89191ca
Павел Шишковец <pavelsch00@gmail.com>
2020-07-10 17:53:09 +0300

    Project type has been changed to library

---
+++ Task1Library/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Task1Library
+{
+    public class Class1
+    {
+    }
+}
---
+++ Task1_1/Class1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task1_1
+{
+    /// <summary>
+    ///  Class CGD.
+    ///   Implements the Euclidean algorithm or Binary GCD algorithm.
+    /// </summary>
+    public static class CGD
+    {
+        /// <summary>
+        ///  Method EuclideanAlgorithm.
+        ///   Finds the GCD of two integers.
+        ///   Input data: int, int.
+        ///   Output data: int result, double algorithmExecutionTime.
+        /// </summary>
+        public static (int result, double algorithmExecutionTime) EuclideanAlgorithm(int number1, int number2)
+        {
+            DateTime StartAlgorithmTime = DateTime.Now;
+
+            if (number1 < 0)
+                number1 = Math.Abs(number1);
+
+            if (number2 < 0)
+                number2 = Math.Abs(number2);
+
+            while (number2 != 0)
+            {
+                int temp = number2;
+                number2 = number1 % number2;
+                number1 = temp;
+            }
+
+            return (number1, (DateTime.Now - StartAlgorithmTime).TotalSeconds);
+        }
+
+        /// <summary>
+        ///  Method EuclideanAlgorithm.
+        ///   Finds the GCD of three integers.
+        ///   Input data: int, int, int.
+        ///   Output data: int result.
+        /// </summary>
+        public static int EuclideanAlgorithm(int number1, int number2, int number3) => EuclideanAlgorithm(EuclideanAlgorithm(number1, number2).result, number3).result;
+
+        /// <summary>
+        ///  Method EuclideanAlgorithm.
+        ///   Finds the GCD of four integers.
+        ///   Input data: int, int, int, int.
+        ///   Output data: int result.
+        /// </summary>
+        public static int EuclideanAlgorithm(int number1, int number2, int number3, int number4) => EuclideanAlgorithm(EuclideanAlgorithm(number1, number2).result, EuclideanAlgorithm(number3, number4).result).result;
+
+        /// <summary>
+        ///  Method EuclideanAlgorithm.
+        ///   Finds the GCD of five integers.
+        ///   Input data: int, int, int, int, int.
+        ///   Output data: int result.
+        /// </summary>
+        public static int EuclideanAlgorithm(int number1, int number2, int number3, int number4, int number5) => EuclideanAlgorithm(EuclideanAlgorithm(number1, number2, number3, number4), number5).result;
+
+        /// <summary>
+        ///  Method BinaryGCD.
+        ///   Finds the Binary GCD of two integers and calculates the execution time of the algorithm.
+        ///   Input data: int, int.
+        ///   Output data: int result, double algorithmExecutionTime.
+        /// </summary>
+        public static (int result, double algorithmExecutionTime) BinaryGCD(int number1, int number2)
+        {
+            DateTime StartAlgorithmTime = DateTime.Now;
+
+            if (number1 < 0)
+                number1 = Math.Abs(number1);
+
+            if (number2 < 0)
+                number2 = Math.Abs(number2);
+
+
+            if (number1 == 0)
+                return (number2, (DateTime.Now - StartAlgorithmTime).TotalSeconds);
+            if (number2 == 0)
+                return (number1, (DateTime.Now - StartAlgorithmTime).TotalSeconds);
+
+            int i = 0;
+            while (((number1 | number2) & 1) == 0)
+            {
+                number1 >>= 1;
+                number2 >>= 1;
+                ++i;
+            }
+
+            while ((number1 & 1) == 0)
+                number1 >>= 1;
+
+            do
+            {
+                while ((number2 & 1) == 0)
+                    number2 >>= 1;
+
+                if (number1 > number2)
+                {
+                    int temp = number1;
+                    number1 = number2;
+                    number2 = temp;
+                }
+
+                number2 -= number1;
+            } while (number2 != 0);
+
+            return (number1 << i, (DateTime.Now - StartAlgorithmTime).TotalSeconds);
+        }
+
+        /// <summary>
+        ///  Method GetDataForHistogram.
+        ///  Method preparing data for building bar charts.
+        ///  Input data: double, double.
+        ///  Output data: Dictionary<double, string>.
+        /// </summary>
+        public static Dictionary<double, string> GetDataForHistogram(double executionTimeOfFirstAlgorithm, double executionTimeOfSecondAlgorithm)
+        {
+            Dictionary<double, string> algorithmRuntime = new Dictionary<double, string>(2);
+            algorithmRuntime.Add(executionTimeOfFirstAlgorithm, "Euclidean Algorithm");
+            algorithmRuntime.Add(executionTimeOfSecondAlgorithm, "Binary GCD");
+            return algorithmRuntime;
+        }
+    }
+}
