177f6044237d864d9f9e9fc6389db1c332d7c907
Павел Шишковец <pavelsch00@gmail.com>
2020-07-10 19:37:03 +0300

    Task1: Added 30 unit tests Task 1 completed

--- Task1_1.Tests/Task1_1Tests.cs
+++ Task1_1.Tests/Task1_1Tests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Task1_1.Tests
@@ -156,7 +157,7 @@ namespace Task1_1.Tests
         }
 
         [Fact]
-        public void EuclideanAlgorithmCheckTime_0andMinus99_0returned()
+        public void EuclideanAlgorithmTimeCheck_0andMinus99_0returned()
         {
             // arrange
             int firstNumber = 0;
@@ -171,7 +172,7 @@ namespace Task1_1.Tests
         }
 
         [Fact]
-        public void EuclideanAlgorithmCheckTime_minus99and0_0returned()
+        public void EuclideanAlgorithmTimeCheck_minus99and0_0returned()
         {
             // arrange
             int firstNumber = -99;
@@ -199,6 +200,279 @@ namespace Task1_1.Tests
 
             // assert;
             Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void EuclideanAlgorithm_0andMinus9and90_9returned()
+        {
+            // arrange
+            int firstNumber = 0;
+            int secondNumber = -9;
+            int thirdNumber = 90;
+            int expected = 9;
+
+            // act
+            double actual = CGD.EuclideanAlgorithm(firstNumber, secondNumber, thirdNumber);
+
+            // assert;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void EuclideanAlgorithm_128andMinus64and16and32_16returned()
+        {
+            // arrange
+            int firstNumber = 128;
+            int secondNumber = -64;
+            int thirdNumber = 16;
+            int fourthNumber = 32;
+            int expected = 16;
+
+            // act
+            double actual = CGD.EuclideanAlgorithm(firstNumber, secondNumber, thirdNumber, fourthNumber);
+
+            // assert;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void EuclideanAlgorithm_0and0and0and0_0returned()
+        {
+            // arrange
+            int firstNumber = 0;
+            int secondNumber = 0;
+            int thirdNumber = 0;
+            int fourthNumber = 0;
+            int expected = 0;
+
+            // act
+            double actual = CGD.EuclideanAlgorithm(firstNumber, secondNumber, thirdNumber, fourthNumber);
+
+            // assert;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void EuclideanAlgorithm_0and9and27and0_9returned()
+        {
+            // arrange
+            int firstNumber = 0;
+            int secondNumber = 9;
+            int thirdNumber = 27;
+            int fourthNumber = 0;
+            int expected = 9;
+
+            // act
+            double actual = CGD.EuclideanAlgorithm(firstNumber, secondNumber, thirdNumber, fourthNumber);
+
+            // assert;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void EuclideanAlgorithm_0and9and27andMinus8_1returned()
+        {
+            // arrange
+            int firstNumber = 0;
+            int secondNumber = 9;
+            int thirdNumber = 27;
+            int fourthNumber = -8;
+            int expected = 1;
+
+            // act
+            double actual = CGD.EuclideanAlgorithm(firstNumber, secondNumber, thirdNumber, fourthNumber);
+
+            // assert;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void EuclideanAlgorithm_0and0and0and0and0_0returned()
+        {
+            // arrange
+            int firstNumber = 0;
+            int secondNumber = 0;
+            int thirdNumber = 0;
+            int fourthNumber = 0;
+            int fifthNumberNumber = 0;
+            int expected = 0;
+
+            // act
+            double actual = CGD.EuclideanAlgorithm(firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumberNumber);
+
+            // assert;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void EuclideanAlgorithm_117and3and9and0and27_0returned()
+        {
+            // arrange
+            int firstNumber = 117;
+            int secondNumber = 3;
+            int thirdNumber = 9;
+            int fourthNumber = 0;
+            int fifthNumberNumber = 27;
+            int expected = 3;
+
+            // act
+            double actual = CGD.EuclideanAlgorithm(firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumberNumber);
+
+            // assert;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void EuclideanAlgorithm_minus117and3and9and0andMinus27_0returned()
+        {
+            // arrange
+            int firstNumber = -117;
+            int secondNumber = 3;
+            int thirdNumber = 9;
+            int fourthNumber = 0;
+            int fifthNumberNumber = -27;
+            int expected = 3;
+
+            // act
+            double actual = CGD.EuclideanAlgorithm(firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumberNumber);
+
+            // assert;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void BinaryGCD_27and99_9returned()
+        {
+            // arrange
+            int firstNumber = 27;
+            int secondNumber = 99;
+            int expected = 9;
+
+            // act
+            double actual = CGD.EuclideanAlgorithm(firstNumber, secondNumber).result;
+
+            // assert;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void BinaryGCD_0and13_13returned()
+        {
+            // arrange
+            int firstNumber = 0;
+            int secondNumber = 13;
+            int expected = 13;
+
+            // act
+            double actual = CGD.EuclideanAlgorithm(firstNumber, secondNumber).result;
+
+            // assert;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void BinaryGCD_15and0_15returned()
+        {
+            // arrange
+            int firstNumber = 15;
+            int secondNumber = 0;
+            int expected = 15;
+
+            // act
+            double actual = CGD.EuclideanAlgorithm(firstNumber, secondNumber).result;
+
+            // assert;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void BinaryGCD_0and0_0returned()
+        {
+            // arrange
+            int firstNumber = 0;
+            int secondNumber = 0;
+            int expected = 0;
+
+            // act
+            double actual = CGD.EuclideanAlgorithm(firstNumber, secondNumber).result;
+
+            // assert;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void BinaryGCD_minus18and36_18returned()
+        {
+            // arrange
+            int firstNumber = -18;
+            int secondNumber = 36;
+            int expected = 18;
+
+            // act
+            double actual = CGD.EuclideanAlgorithm(firstNumber, secondNumber).result;
+
+            // assert;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void BinaryGCD_minus18andMinus36_2returned()
+        {
+            // arrange
+            int firstNumber = -28;
+            int secondNumber = -86;
+            int expected = 2;
+
+            // act
+            double actual = CGD.EuclideanAlgorithm(firstNumber, secondNumber).result;
+
+            // assert;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void BinaryGCD_1and27_1returned()
+        {
+            // arrange
+            int firstNumber = 1;
+            int secondNumber = 27;
+            int expected = 1;
+
+            // act
+            double actual = CGD.EuclideanAlgorithm(firstNumber, secondNumber).result;
+
+            // assert;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void BinaryGCDTimeCheck_minus99and0_0returned()
+        {
+            // arrange
+            int firstNumber = -99;
+            int secondNumber = 0;
+            int expected = 0;
+
+            // act
+            double actual = CGD.BinaryGCD(firstNumber, secondNumber).algorithmExecutionTime;
+
+            // assert;
+            Assert.True(expected < actual);
+        }
+
+        [Fact]
+        public void BinaryGCDTimeCheck_minus99and9_9returned()
+        {
+            // arrange
+            int firstNumber = 99;
+            int secondNumber = 9;
+            int expected = 0;
+
+            // act
+            double actual = CGD.BinaryGCD(firstNumber, secondNumber).algorithmExecutionTime;
+
+            // assert;
+            Assert.True(expected < actual);
         }
     }
 }
