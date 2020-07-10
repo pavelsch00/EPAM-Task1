c4afbcfa7b9923c4aa067eddd244f1feff1d77a5
Павел Шишковец <pavelsch00@gmail.com>
2020-07-10 16:45:28 +0300

    Task 1: in sub-task two, an algorithm runtime counter has been added

--- Task1/EuclideanAlgorithm.cs
+++ Task1/EuclideanAlgorithm.cs
@@ -5,20 +5,26 @@ using System.Text;
 namespace Task1
 {
     /// <summary>
-    ///  Class EuclideanAlgorithm.
-    ///   Implements the Euclidean algorithm.
+    ///  Class CGD.
+    ///   Implements the Euclidean algorithm or Binary GCD algorithm.
     /// </summary>
-    public static class EuclideanAlgorithm
+    public static class CGD
     {
         /// <summary>
-        ///  Method CalculationOfCGD.
+        ///  Method EuclideanAlgorithm.
         ///   Finds the GCD of two integers.
         ///   Input data: int, int.
         ///   Output data: int.
         /// </summary>
-        public static int CalculationOfGCD(int number1, int number2)
+        public static int EuclideanAlgorithm(int number1, int number2)
         {
-            while(number2 != 0)
+            if (number1 < 0)
+                number1 = Math.Abs(number1);
+
+            if (number2 < 0)
+                number2 = Math.Abs(number2);
+
+            while (number2 != 0)
             {
                 int temp = number2;
                 number2 = number1 % number2;
@@ -29,27 +35,78 @@ namespace Task1
         }
 
         /// <summary>
-        ///  Method CalculationOfCGD.
+        ///  Method EuclideanAlgorithm.
         ///   Finds the GCD of three integers.
         ///   Input data: int, int, int.
         ///   Output data: int.
         /// </summary>
-        public static int CalculationOfGCD(int number1, int number2, int number3) => CalculationOfGCD(CalculationOfGCD(number1, number2), number3);
+        public static int EuclideanAlgorithm(int number1, int number2, int number3) => EuclideanAlgorithm(EuclideanAlgorithm(number1, number2), number3);
 
         /// <summary>
-        ///  Method CalculationOfCGD.
+        ///  Method EuclideanAlgorithm.
         ///   Finds the GCD of four integers.
         ///   Input data: int, int, int, int.
         ///   Output data: int.
         /// </summary>
-        public static int CalculationOfGCD(int number1, int number2, int number3, int number4) => CalculationOfGCD(CalculationOfGCD(number1, number2), CalculationOfGCD(number3, number4));
+        public static int EuclideanAlgorithm(int number1, int number2, int number3, int number4) => EuclideanAlgorithm(EuclideanAlgorithm(number1, number2), EuclideanAlgorithm(number3, number4));
 
         /// <summary>
-        ///  Method CalculationOfCGD.
+        ///  Method EuclideanAlgorithm.
         ///   Finds the GCD of five integers.
         ///   Input data: int, int, int, int, int.
         ///   Output data: int.
         /// </summary>
-        public static int CalculationOfGCD(int number1, int number2, int number3, int number4, int number5) => CalculationOfGCD(CalculationOfGCD(number1, number2, number3, number4), number5);
+        public static int EuclideanAlgorithm(int number1, int number2, int number3, int number4, int number5) => EuclideanAlgorithm(EuclideanAlgorithm(number1, number2, number3, number4), number5);
+
+        /// <summary>
+        ///  Method BinaryGCD.
+        ///   Finds the Binary GCD of two integers and calculates the execution time of the algorithm.
+        ///   Input data: int, int.
+        ///   Output data: int result, double AlgorithmExecutionTime.
+        /// </summary>
+        public static (int result, double AlgorithmExecutionTime) BinaryGCD(int number1, int number2)
+        {
+            DateTime StartAlgorithmTime = DateTime.Now;
+
+            if (number1 < 0)
+                number1 = Math.Abs(number1);
+
+            if (number2 < 0)
+                number2 = Math.Abs(number2);
+
+
+            if (number1 == 0)
+                return (number2, (DateTime.Now - StartAlgorithmTime).TotalSeconds);
+            if (number2 == 0)
+                return (number1, (DateTime.Now - StartAlgorithmTime).TotalSeconds);
+
+            int i = 0;
+            while (((number1 | number2) & 1) == 0)
+            {
+                number1 >>= 1;
+                number2 >>= 1;
+                ++i;
+            }
+
+            while ((number1 & 1) == 0)
+                number1 >>= 1;
+
+            do
+            {
+                while ((number2 & 1) == 0)
+                    number2 >>= 1;
+
+                if (number1 > number2)
+                {
+                    int temp = number1;
+                    number1 = number2;
+                    number2 = temp;
+                }
+
+                number2 -= number1;
+            } while (number2 != 0);
+
+            return (number1 << i, (DateTime.Now - StartAlgorithmTime).TotalSeconds);
+        }
     }
 }
