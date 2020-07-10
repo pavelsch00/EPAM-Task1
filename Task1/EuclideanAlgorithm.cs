b7fb461c9b2296e3f0fcc6f073402a6d72cc58a4
Павел Шишковец <pavelsch00@gmail.com>
2020-07-10 15:02:08 +0300

    Task1: made the first point

---
+++ Task1/EuclideanAlgorithm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    /// <summary>
+    ///  Class EuclideanAlgorithm.
+    ///   Implements the Euclidean algorithm.
+    /// </summary>
+    public class EuclideanAlgorithm
+    {
+        /// <summary>
+        ///  Method CalculationOfCGD.
+        ///   Finds the GCD of two integers.
+        ///   Input data: int, int.
+        ///   Output data: int.
+        /// </summary>
+        public static int CalculationOfGCD(int number1, int number2)
+        {
+            while(number2 != 0)
+            {
+                int temp = number2;
+                number2 = number1 % number2;
+                number1 = temp;
+            }
+
+            return number1;
+        }
+    }
+}
